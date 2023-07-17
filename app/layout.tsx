a45327fcf8b7fe2a055951ee842fef1c7fe25ecd
WHRCT22 <125719278+WHRCT22@users.noreply.github.com>
2023-07-17 17:45:39 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import "./styles/highlight.scss";
 import { getClientConfig } from "./config/client";
 
 export const metadata = {
-  title: "ChatGPT Next Web",
+  title: "WHR -- ChatGPT",
   description: "Your personal ChatGPT Chat Bot.",
   viewport: {
     width: "device-width",
